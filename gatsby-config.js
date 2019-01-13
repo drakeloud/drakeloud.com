0d9d8891559c0c5617d003791f4396c406572386
Loud, Drake <Drake.Loud2@T-Mobile.com>
2019-01-13 12:56:22 -0800

    Added contenful and setup sass, dotenv

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+require(`dotenv`).config({ path: `.env` });
+
+module.exports = {
+    siteMetadata: {
+        name: `Test Blog site`,
+        tagline: `"Do, or do not. There is no try."`,
+        githubLink: `https://github.com`
+    },
+    plugins: [
+        `gatsby-plugin-sass`,
+        `gatsby-plugin-typescript`,
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_DELIVERY_ACCESS_TOKEN
+            }
+        }
+    ]
+};
